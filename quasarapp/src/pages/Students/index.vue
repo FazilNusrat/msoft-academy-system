58c3697f729395f95046a11c0c13dff5b873bb47
faisalnusrat8182 <faisalnusrat57@gmail.com>
2021-05-04 21:29:25 -0700

    edit
    edit

--- quasarapp/src/pages/Students/index.vue
+++ quasarapp/src/pages/Students/index.vue
@@ -1,33 +1,34 @@
 <template>
-  <div student="q-pa-md q-gutter-sm ">
-      <div class="row">
+  <div class="q-ma-sm my_radio_less three_d q-pa-xs">
+    <!-- <q-card class="bg-teal text-white" style="width: 300px">
+        <q-card-section>
+          <div class="text-h6">Add Class</div>
+        </q-card-section> -->
 
-        <div class="col">
-          <div class="q-ma-xs  q-pa-xs">  
-
-    <h-title value="0.14" >Add Student</h-title>
-    </div>
-    <div class="row justify-between">
+    <h-title>Entry Class</h-title>
+    <div class="row justify-between q-mt-sm">
       <div class="row">
-        <l-button icon="add" color="red" @click="medium = true">Add New</l-button>
+        <l-button icon="add" color="red" @click="medium = true"
+          >Add New</l-button
+        >
         <l-button icon="mdi-file-pdf" color="orange">PDF</l-button>
-        <l-button icon="mdi-microsoft-excel" color="green-10">Excel</l-button>
+        <l-button @click="addModal" icon="mdi-microsoft-excel" color="green-10">Excel</l-button>
         <l-button icon="mdi-email-send" color="red-6">Email</l-button>
         <l-button icon="mdi-whatsapp" color="green-6">Whatsapp</l-button>
       </div>
       <div class="row">
-        <l-button icon="mdi-database-search" color="blue-grey-9">Advance Search</l-button>
+        <l-button icon="mdi-database-search" color="blue-grey-9"
+          >Advance Search</l-button
+        >
         <l-button icon="mdi-database-import" color="blue-7">Import</l-button>
       </div>
     </div>
-        </div>
-      </div>
 
     <div>
       <n-table
         :loading="loading"
         @head="head"
-        :data="studentData"
+        :data="classdata"
         :pagination.sync="pagination"
         @del="del"
         @info="info"
@@ -36,67 +37,46 @@
         :columns="columns"
         @request="onRequest"
       />
+
+      <m-modal :showCM.sync="showAddModal">
+       <n-add-modal @close="hideAddModal()" />
+    </m-modal>
+    <m-modal :showCM.sync="showEditModal">
+      <n-edit-modal :id="id" @close="hideEditModal()" />
+    </m-modal>
+    <m-modal :showCM.sync="showInfoModal">
+      <n-info-modal :id="id" @close="hideInfoModal()" />
+    </m-modal>
     </div>
     <q-dialog v-model="medium">
       <q-card style="width: 700px; width: 80vw">
-        <q-card-section student="bg-teal text-white">
-          <div student="text-h6">Add Student</div>
+        <q-card-section class="bg-teal text-white">
+          <div class="text-h6">Add Class</div>
         </q-card-section>
 
-        <q-card-section student="q-pt-none">
-          <div student="row">
-            <div student="col"></div>
-            <div student="col-10 q-mr-lg">
+        <q-card-section class="q-pt-none">
+          <div class="row">
+            <div class="col"></div>
+            <div class="col-10 q-mr-lg">
               <q-input color="teal" v-model="form.name" label="Name">
                 <template v-slot:prepend>
                   <q-icon name="add" />
                 </template>
               </q-input>
-              <q-input color="teal" v-model="form.last_name" label="Last Name">
+              <q-input
+                color="teal"
+                v-model="form.description"
+                label="Description"
+              >
                 <template v-slot:prepend>
-                  <q-icon name="add" />
-                </template>
-              </q-input>
-              <q-input color="teal" v-model="form.father_name" label="Father Name">
-                <template v-slot:prepend>
-                  <q-icon name="add" />
-                </template>
-              </q-input>
-              <q-input color="teal" tye="email" v-model="form.email" label="Email">
-                <template v-slot:prepend>
-                  <q-icon name="add" />
-                </template>
-              </q-input>
-              <q-input color="teal" type="number" v-model="form.cnic" label="CNIC">
-                <template v-slot:prepend>
-                  <q-icon name="add" />
-                </template>
-              </q-input>
-              <q-input color="teal" type="number" v-model="form.phone" label="Phone">
-                <template v-slot:prepend>
-                  <q-icon name="add" />
-                </template>
-              </q-input>
-              <q-input color="teal" v-model="form.salary" label="Salary">
-                <template v-slot:prepend>
-                  <q-icon name="add" />
-                </template>
-              </q-input>
-              <q-input color="teal" v-model="form.address" label="Address">
-                <template v-slot:prepend>
-                  <q-icon name="add" />
-                </template>
-              </q-input>
-              <q-input color="teal" v-model="form.regint" label="Regint">
-                <template v-slot:prepend>
-                  <q-icon name="add" />
+                  <q-icon name="info" />
                 </template>
               </q-input>
             </div>
           </div>
         </q-card-section>
 
-        <q-card-actions align="right" student="bg-white text-teal">
+        <q-card-actions align="right" class="bg-white text-teal">
           <q-btn flat label="OK" @click="SaveRecord" v-close-popup>
             <q-icon name="save" />
           </q-btn>
@@ -110,17 +90,30 @@
 </template>
 
 <script>
+
 import NTable from "../../components/tables/DataTable.vue";
 import LButton from "../../components/Buttons/LinearButton.vue";
 import HTitle from "../../components/Headers/HeaderTitle.vue";
+import MModal from 'src/components/general-components/MainModal.vue';
+import NAddModal from 'src/components/modals/class/Add.vue';
+import NEditModal from 'src/components/modals/class/Edit.vue';
+import NInfoModal from 'src/components/modals/class/Info.vue';
 
 export default {
-  components: { NTable, LButton, HTitle},
+  components: { NTable, LButton, HTitle, MModal, NAddModal, NEditModal, NInfoModal },
 
   data() {
     return {
-      selectedStudent:null,
-      studentList: [],
+      getP:null,
+      visible: true,
+      loading: false,
+      id: 0,
+      showAddModal: false,
+      showEditModal: false,
+      showInfoModal: false,
+      selectedClass: null,
+      // classList: ['CS-Morning', 'IT-Evening'],
+      classList: [],
       columns: [
         {
           name: "id",
@@ -128,98 +121,35 @@ export default {
           label: this.$t("Number"),
           field: (row) => row.id,
           sortable: true,
-          studentes: "bg-grey-2 ellipsis my_width10",
+          classes: "bg-grey-2 ellipsis my_width10",
           align: "center",
-          headerstudentes: "bg-light-blue-6 text-white ",
+          headerClasses: "bg-light-blue-6 text-white ",
         },
         {
           name: "name",
-          student: "my_width20 bg-grey-2",
+          classes: "my_width20 bg-grey-2",
           align: "left",
+          // label: this.$t("Name"),
           label: "Name",
           field: (row) => row.name,
           sortable: true,
         },
         {
-          name: "last_name",
-          student: "bg-grey-2 ellipsis my_width20",
+          name: "description",
+          classes: "bg-grey-2 ellipsis my_width20",
           // label: this.$t("ContactPerson"),
-          label: "Last name",
+          label: "Description",
           align: "left",
-          field: (row) => row.last_name,
+          field: (row) => row.description,
           sortable: true,
         },
-        {
-          name: "father_name",
-          student: "bg-grey-2 ellipsis my_width20",
-          // label: this.$t("ContactPerson"),
-          label: "Father Name",
-          align: "left",
-          field: (row) => row.father_name,
-          sortable: true,
-        },
-        {
-          name: "email",
-          student: "bg-grey-2 ellipsis my_width20",
-          // label: this.$t("ContactPerson"),
-          label: "Email",
-          align: "left",
-          field: (row) => row.email,
-          sortable: true,
-        },
-        {
-          name: "cnic",
-          student: "bg-grey-2 ellipsis my_width20",
-          // label: this.$t("ContactPerson"),
-          label: "CNIC",
-          align: "left",
-          field: (row) => row.cnic,
-          sortable: true,
-        },
-        {
-          name: "phone",
-          student: "bg-grey-2 ellipsis my_width20",
-          // label: this.$t("ContactPerson"),
-          label: "Phone",
-          align: "left",
-          field: (row) => row.phone,
-          sortable: true,
-        },
-        {
-          name: "salry",
-          student: "bg-grey-2 ellipsis my_width20",
-          // label: this.$t("ContactPerson"),
-          label: "Salry",
-          align: "left",
-          field: (row) => row.Phone,
-          sortable: true,
-        },
-        {
-          name: "address",
-          student: "bg-grey-2 ellipsis my_width20",
-          // label: this.$t("ContactPerson"),
-          label: "Address",
-          align: "left",
-          field: (row) => row.address,
-          sortable: true,
-        },
-        {
-          name: "regint",
-          student: "bg-grey-2 ellipsis my_width20",
-          // label: this.$t("ContactPerson"),
-          label: "Regint",
-          align: "left",
-          field: (row) => row.regint,
-          sortable: true,
-        },
-        
 
         {
           name: "actions",
           label: this.$t("Actions"),
           align: "center",
           sortable: false,
-          studentes: "bg-grey-2 my_width10",
+          classes: "bg-grey-2 my_width10",
         },
       ],
       loading: false,
@@ -229,7 +159,6 @@ export default {
       page: 1,
       rowsPerPage: 12,
       rowsNumber: 12,
-      data: [],
       pagination: {
         sortBy: "created_at",
         descending: false,
@@ -238,24 +167,19 @@ export default {
         rowsNumber: 12,
       },
       medium: false,
-      studentData: [],
+      classdata: [],
       form: {
         name: null,
-        last_name: null,
-        father_name: null,
-        email: null,
-        cnic: null,
-        phone: null,
-        salary: null,
-        address: null,
-        regint: null,
+        description: null,
       },
     };
   },
   methods: {
     SaveRecord() {
+      this.visible = true;
+      this.loading = true;
       // console.log("Test File", this.form.name);
-      this.$axios.post("student/store", this.form).then((res) => {
+      this.$axios.post("subject/store", this.form).then((res) => {
         this.$q.notify({
           color: "green-4",
           textColor: "white",
@@ -264,58 +188,87 @@ export default {
           message: "Successfully inserted",
         });
         this.clear();
-        // this.$router.push("/student/index");
-        this.getdata();
+        // this.$router.push("/class/index");
+        this.getRecord();
       });
     },
     clear() {
-      (this.form.name = ""), 
-      (this.form.last_name = "");
-      (this.form.father_name = "");
-      (this.form.email = "");
-      (this.form.cnic = "");
-      (this.form.phone = "");
-      (this.form.salary = "");
-      (this.form.address = "");
-      (this.form.regint = "");
+      (this.form.name = ""), (this.form.description = "");
     },
-    getdata() {
-      this.$axios.get("student/display", this.studentData).then((Response) => {
-        this.studentData = Response.data;
-
-      this.$axios.get("student/display", student).then((Response) => {
-        student = Response.data;
-
+    getRecord() {
+      let p = this.getP;
+      this.visible = true;
+      this.loading = true;
+      this.$axios.get('subject/display'+
+      '?current_page='+
+      p.pagination.page+'&per_page='+p.pagination.rowsPerPage+'&filter='+this.filter+'&sort_by='+p.pagination.sortBy+'&descending='+this.pagination.descending).then(res=>{
+      this.show = false;
+      this.visible = false;
+      this.loading = false;
+      this.classdata = res.data;
+      // this.classdata = res.data.data;
+      this.pagination.page = res.data.current_page;
+      this.pagination.rowsPerPage = res.data.per_page;
+      this.pagination.rowsNumber = res.data.total;
+      }).catch(error=>{
 
     })
-      });
+      // this.$axios.get("class/display", this.classdata).then((Response) => {
+      //   this.classdata = Response.data;
+      // });
     },
     head(name) {
       if (this.pagination.descending) this.pagination.descending = true;
       else this.pagination.descending = true;
       this.pagination.sortBy = name;
     },
-  
 
-  del(id = 0) {
-    console.log("dels: ", id);
-  },
-  info(id = 0) {
-    this.$router.push("/customer/show/" + id);
-  },
-  onRequest(props) {
-    // console.log("propss: ", props);
-    this.getProp = props;
-    this.getRecord();
-  },
-  
-edit (id=0) {
-        this.$router.push('/customer/edit/'+id);
+    del(id = 0) {
+      this.id = id;
+      this.showEditModal = true;
+    },
+    info(id = 0) {
+      this.id = id;
+      this.showInfoModal = true;
+    },
+    onRequest(props) {
+      // console.log("propss: ", props);
+      this.getProp = props;
+      this.getRecord();
+    },
+
+    edit(id = 0) {
+      this.id = id;
+      this.showEditModal = true;
+    },
+    addModal() {
+      // this.form.name = null;
+      this.showAddModal = true;
+    },
+    hideAddModal() {
+      this.showAddModal = false;
+      this.getRecord()
+    },
+    hideEditModal() {
+      this.showEditModal = false;
+      this.getRecord()
+    },
+    hideInfoModal() {
+      this.showInfoModal = false;
+      this.getRecord()
+    },
+    onRequest(props) {
+       this.getP = props;
+      this.getRecord();
     },
   },
 
   created() {
-    this.getdata();
+    // this.getRecord();
+    this.onRequest({
+      pagination: this.pagination,
+      filter: undefined
+    });
   },
 };
 </script>
