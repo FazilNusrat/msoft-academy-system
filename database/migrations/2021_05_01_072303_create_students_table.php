620c186973d679adf34a4863c2ef0182871d5e5e
0786775131 <83202431+0786775131@users.noreply.github.com>
2021-06-07 15:15:45 +0430

    imran
    Salve The problem

--- database/migrations/2021_05_01_072303_create_students_table.php
+++ database/migrations/2021_05_01_072303_create_students_table.php
@@ -22,18 +22,9 @@ class CreateStudentsTable extends Migration
             $table->string                  ('class_id')                ->nullable();
             $table->string                  ('section_id')              ->nullable();
             $table->date                    ('date_of_birth')           ->nullable();
-            $table->string                  ('category')                ->nullable();
-            $table->string                  ('religion')                ->nullable();
-            $table->string                  ('caste')                   ->nullable();
             $table->integer                 ('mobile_number')           ->nullable();
             $table->string                  ('email')                   ->nullable();
             $table->date                    ('addmission_Date')         ->nullable();
-            $table->integer                 ('blood_group_id')          ->nullable();
-            $table->string                  ('student_house')           ->nullable();
-            $table->integer                 ('height')                  ->nullable();
-            $table->integer                 ('weight')                  ->nullable();
-            $table->date                    ('as_on_date')              ->nullable();
-            $table->string                  ('medical_history')         ->nullable();
             $table->string                  ('gender')                  ->nullable();
             $table->string                  ('image')                   ->nullable();
             $table->json                    ('parent_details')          ->nullable();
